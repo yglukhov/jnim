56bb40e513bfada49e0f86a883cb8fc7706f4a80
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2016-06-24 15:18:44 +0600

    Collection and List update

--- src/javaapi/containers.nim
+++ src/javaapi/containers.nim
@@ -1,12 +1,45 @@
 import jbridge,
        core
 
+jclass java.util.Iterator*[V] of JVMObject:
+  proc hasNext*[V]: bool
+  proc next*[V]: V
+  proc remove*[V]
+  
 jclass java.util.Collection*[V] of JVMObject:
   proc add*[V](v: V): bool
+  proc addAll*[V](c: Collection[V]): bool
+  proc clear*[V]
+  proc contains*[V](o: JVMObject): bool
+  proc hasCode*[V]: jint
+  proc isEmpty*[V]: bool
+  proc toIterator*[V]: Iterator[V] {.importc: "iterator".}
+  proc remove*[V,U](o: U): bool
+  proc removeAll*[V,U](o: Collection[U]): bool
+  proc retainAll*[V,U](o: Collection[U]): bool
+  proc size*[V]: jint
 
 jclass java.util.List*[V] of Collection[V]:
   proc add*[V](i: jint, v: V)
   proc get*[V](i: jint): V
+  proc set*[V](i: jint): V
+  proc remove*[V](i: jint)
+  proc subList*[V](f, t: jint): List[V]
 
 jclass java.util.ArrayList*[V] of List[V]:
   proc new*[V]
+
+proc new*[V](t: typedesc[ArrayList[V]], c: openArray[V]): ArrayList[V] =
+  result = ArrayList[V].new
+  for v in c:
+    discard result.add(v)
+
+#################################################################################################### 
+# Helpers
+
+proc toSeq*[V](c: Collection[V]): seq[V] =
+  result = newSeq[V]()
+  let it = c.toIterator
+  while it.hasNext:
+    result.add it.next
+
--- tests/test_javaapi_containers.nim
+++ tests/test_javaapi_containers.nim
@@ -9,4 +9,17 @@ suite "javaapi.containers":
       initJNIForTests()
 
   test "javaapi.containers - List":
-    let l = ArrayList[string].new()
+    let xs = ArrayList[string].new()
+    discard xs.add("Hello")
+    xs.add(1, "world")
+    check: xs.get(0) == "Hello"
+    check: xs.get(1) == "world"
+    expect JavaException:
+      discard xs.get(3)
+    var s = newSeq[string]()
+    let it = xs.toIterator
+    while it.hasNext:
+      s.add it.next
+    check: s == @["Hello", "world"]
+    discard xs.removeAll(ArrayList[string].new(["world", "!"]))
+    check: xs.toSeq == @["Hello"]
